50c16b78e7aa2a3637ae99ab9c635b8a93659f7d
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-07-07 19:42:49 +0300

    Add create recipe

--- lesson-03/base/src/app.js
+++ lesson-03/base/src/app.js
@@ -1,53 +1,9 @@
-async function getRecipes() {
-    const response = await fetch('http://localhost:3030/jsonstore/cookbook/recipes');
-    const recipes = await response.json();
+window.addEventListener('load', start)
 
-    return Object.values(recipes);
-}
-
-async function getRecipeById(id) {
-    const response = await fetch('http://localhost:3030/jsonstore/cookbook/details/' + id);
-    const recipe = await response.json();
-
-    return recipe;
-}
-
-function createRecipePreview(recipe) {
-    const result = e('article', { className: 'preview', onClick: toggleCard },
-        e('div', { className: 'title' }, e('h2', {}, recipe.name)),
-        e('div', { className: 'small' }, e('img', { src: recipe.img })),
-    );
-
-    return result;
-
-    async function toggleCard() {
-        const fullRecipe = await getRecipeById(recipe._id);
-
-        result.replaceWith(createRecipeCard(fullRecipe));
-    }
-}
-
-function createRecipeCard(recipe) {
-    const result = e('article', {},
-        e('h2', {}, recipe.name),
-        e('div', { className: 'band' },
-            e('div', { className: 'thumb' }, e('img', { src: recipe.img })),
-            e('div', { className: 'ingredients' },
-                e('h3', {}, 'Ingredients:'),
-                e('ul', {}, recipe.ingredients.map(i => e('li', {}, i))),
-            )
-        ),
-        e('div', { className: 'description' },
-            e('h3', {}, 'Preparation:'),
-            recipe.steps.map(s => e('p', {}, s))
-        ),
-    );
-
-    return result;
-}
-
-window.addEventListener('load', async () => {
+async function start() {
     const main = document.querySelector('main');
+    const p = document.querySelector('p')
+
     const guestNav = document.getElementById('guest');
     const userNav = document.getElementById('user');
 
@@ -68,34 +24,78 @@ window.addEventListener('load', async () => {
     }
 
     const recipes = await getRecipes();
-    const cards = recipes.map(createRecipePreview);
 
-    main.innerHTML = '';
-    cards.forEach(c => main.appendChild(c));
+    recipes.map(createPreview).forEach(el => main.appendChild(el));
 
-});
+    p.remove();
+}
 
-function e(type, attributes, ...content) {
-    const result = document.createElement(type);
+async function togglePreview(id, preview) {
+    const recipe = await getRecipeById(id);
 
-    for (let [attr, value] of Object.entries(attributes || {})) {
-        if (attr.substring(0, 2) == 'on') {
-            result.addEventListener(attr.substring(2).toLocaleLowerCase(), value);
-        } else {
-            result[attr] = value;
+    const element = document.createElement('article');
+
+    element.innerHTML = `<h2>${recipe.name}</h2>
+    <div class="band">
+        <div class="thumb">
+            <img src="${recipe.img}">
+        </div>
+        <div class="ingredients">
+            <h3>Ingredients:</h3>
+            <ul>
+                ${recipe.ingredients.map(i => `<li>${i}</li>`).join('')}
+            </ul>
+        </div>
+    </div>
+    <div class="description">
+        <h3>Preparation:</h3>
+        ${recipe.steps.map(p => `<p>${p}</p>`).join('')}
+    </div>`;
+
+    preview.replaceWith(element);
+}
+
+function createPreview(recipe) {
+    const element = document.createElement('article');
+    element.setAttribute('class', 'preview');
+
+    element.innerHTML = `<div class="title">
+            <h2>${recipe.name}</h2>
+            </div>
+            <div class="small">
+                <img src="${recipe.img}">
+            </div>`;
+
+    element.addEventListener('click', () => {
+        element.querySelector('h2').textContent = 'Loading...'
+        togglePreview(recipe._id, element);
+    })
+
+    return element;
+}
+
+async function getRecipes() {
+    const url = 'http://localhost:3030/jsonstore/cookbook/recipes'
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return Object.values(data);
+}
+
+async function getRecipeById(id) {
+    const url = 'http://localhost:3030/jsonstore/cookbook/details/' + id;
+
+    try {
+        const res = await fetch(url);
+        if (!res.ok) {
+            const error = await res.json();
+            throw error;
         }
+        const data = await res.json();
+
+        return data;
+    } catch (err) {
+        alert(err.message);
     }
-
-    content = content.reduce((a, c) => a.concat(Array.isArray(c) ? c : [c]), []);
-
-    content.forEach(e => {
-        if (typeof e == 'string' || typeof e == 'number') {
-            const node = document.createTextNode(e);
-            result.appendChild(node);
-        } else {
-            result.appendChild(e);
-        }
-    });
-
-    return result;
 }
